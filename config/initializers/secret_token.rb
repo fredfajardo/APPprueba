7ba0d737c77aab89b2e1e735b30343c1eb7df3cd
fredfajardo <fred_301991@hotmail.com>
2014-02-08 17:16:59 -0600

    Checkpoint inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+APPprueba::Application.config.secret_key_base = 'a8741a292feb8c72f9e8b0107f53595eb8dcbd9ca155160ff7f30b02a2aa85935a5e635af3960049d7c65aff1ee265db4fd29989accd792eadea7ffa9b8b68a2'
